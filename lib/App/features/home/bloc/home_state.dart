e2f8945e2fe9d2f031dd339934b6f8f9f621ba35
premnadh <premnathbr123@gmail.com>
2023-10-10 14:36:23 +0530

    homestates

--- lib/App/features/home/bloc/home_state.dart
+++ lib/App/features/home/bloc/home_state.dart
@@ -6,4 +6,12 @@ abstract class HomeActionState{}
 
 class HomeInitial extends HomeState {}
 
+class HomeLoadingState extends HomeState{}
 
+class HomeLoadedSuccessState extends HomeState{}
+
+class HomeErrorState extends HomeState{}
+
+class HomeNavigateToWishListPageActionState extends HomeActionState{}
+
+class HomeNavigateToCartPageActionState extends HomeActionState{}
